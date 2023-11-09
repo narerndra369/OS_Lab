5cbb27cf0e680593c1d032d26eb92876ed875d69
Malapula Narendra <141564331+narerndra369@users.noreply.github.com>
2023-11-09 18:18:03 +0530

    Create FCFS_DISK.c

---
+++ FCFS_DISK.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int t[20], n, i, tohm[20], tot = 0;
+    float avhm;
+
+    printf("Enter the number of tracks: ");
+    scanf("%d", &n);
+
+    printf("Enter the tracks to be traversed:\n");
+    for (i = 1; i <= n; i++)
+        scanf("%d", &t[i]);
+
+    for (i = 1; i < n; i++)
+    {
+        tohm[i] = t[i + 1] - t[i];
+        if (tohm[i] < 0)
+            tohm[i] = tohm[i] * (-1);
+    }
+
+    for (i = 1; i < n; i++)
+        tot += tohm[i];
+
+    avhm = (float)tot / n;
+
+    printf("\nTracks traversed\tDifference between tracks\n");
+    for (i = 1; i < n; i++)
+        printf("%d\t\t\t%d\n", t[i], tohm[i]);
+
+    printf("\nAverage header movements: %f\n", avhm);
+
+    return 0;
+}
